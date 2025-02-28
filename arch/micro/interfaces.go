1203cadccfb36954334f099e42be8df4bdcb7e89
naveenTelebu <developer1@wbg.ae>
2025-03-01 03:59:38 +0530

    Go Code Data

---
+++ arch/micro/interfaces.go
@@ -0,0 +1,42 @@
+package micro
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/nats-io/nats.go/micro"
+	"github.com/unusualcodeorg/goserve/arch/network"
+)
+
+type NatsGroup = micro.Group
+type NatsHandlerFunc = micro.HandlerFunc
+type NatsRequest = micro.Request
+
+type SendMessage interface {
+	Message(data any)
+	Error(err error)
+}
+
+type MessageSender interface {
+	SendNats(req NatsRequest) SendMessage
+}
+
+type BaseController interface {
+	MessageSender
+	network.BaseController
+}
+
+type Controller interface {
+	BaseController
+	MountNats(group NatsGroup)
+	MountRoutes(group *gin.RouterGroup)
+}
+
+type Router interface {
+	network.BaseRouter
+	NatsClient() NatsClient
+	LoadControllers(controllers []Controller)
+}
+
+type Module[T any] interface {
+	network.BaseModule[T]
+	Controllers() []Controller
+}
---
+++ startup/indexes.go
@@ -0,0 +1,18 @@
+package startup
+
+import (
+	auth "github.com/unusualcodeorg/goserve/api/auth/model"
+	blog "github.com/unusualcodeorg/goserve/api/blog/model"
+	contact "github.com/unusualcodeorg/goserve/api/contact/model"
+	user "github.com/unusualcodeorg/goserve/api/user/model"
+	"github.com/unusualcodeorg/goserve/arch/mongo"
+)
+
+func EnsureDbIndexes(db mongo.Database) {
+	go mongo.Document[auth.Keystore](&auth.Keystore{}).EnsureIndexes(db)
+	go mongo.Document[auth.ApiKey](&auth.ApiKey{}).EnsureIndexes(db)
+	go mongo.Document[user.User](&user.User{}).EnsureIndexes(db)
+	go mongo.Document[user.Role](&user.Role{}).EnsureIndexes(db)
+	go mongo.Document[blog.Blog](&blog.Blog{}).EnsureIndexes(db)
+	go mongo.Document[contact.Message](&contact.Message{}).EnsureIndexes(db)
+}
